cd35b47cad39438d97af431c9fa7b5d57e9da896
airaphi888 <157786045+airaphi888@users.noreply.github.com>
2024-09-03 17:39:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def GPT_response(text):
     # 接收回應
-    response = openai.ChatCompletion.create(model="gpt-4", prompt=text, temperature=0.2, max_tokens=500)
+    response = openai.ChatCompletion.create(model="gpt-4", messages=text, temperature=0.2, max_tokens=500)
     print(response)
     # 重組回應
     answer = response['choices'][0]['text'].replace('。','')
